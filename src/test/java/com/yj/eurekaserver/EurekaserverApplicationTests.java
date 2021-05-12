6958fccccf4a67b87ea9f02b1b5381d2cfed98b4
Ganjinajae <wldudwls12@gmail.com>
2021-05-13 00:19:04 +0900

    commit

---
+++ src/test/java/com/yj/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
